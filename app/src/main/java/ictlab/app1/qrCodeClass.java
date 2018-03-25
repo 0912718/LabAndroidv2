30bb8e173c0423603c95b7a6a44eca7e7d79d5ff
Zina <Zina_1905@hotmail.com>
2018-03-25 18:01:21 +0200

    qr code toegevoegd

--- app/src/main/java/ictlab/app1/qrCodeClass.java
+++ app/src/main/java/ictlab/app1/qrCodeClass.java
@@ -1,8 +1,47 @@
 package ictlab.app1;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 /**
  * Created by edgar on 4-3-2018.
  */
 
-public class qrCodeClass {
+public class qrCodeClass extends Activity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_code);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        //Do anything with result here :D
+        Log.w("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        //Resume scanning
+        //mScannerView.resumeCameraPreview(this);
+    }
+
 }
